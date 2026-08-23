294bff2d903ddbd02d51e80107de3453e32d0958
Suryakant Sahoo <suryakantsahoo14021996@gmail.com>
2026-08-23 11:25:47 +0530

    Update 04_advance_analysis.sql
    Calculate the Running total sales by month

--- sql-analytics/04_advance_analysis.sql
+++ sql-analytics/04_advance_analysis.sql
@@ -10,6 +10,8 @@ group by 1,2)
 select * from ranked_products
 where products_rank < 3;
 
+#I have used str_to_date and case statement as "Order Date" coulmn was not standardised data type.So , to make it standardized I have used#
+ 
 /*MoM growth sales*/
 select * from saless limit 1;
 describe saless;
@@ -37,4 +39,31 @@ total_Sales,
 lag(total_sales) over (order by sales_year , sales_month) as previous_sales,
 round((total_Sales - lag(total_sales) over (order by sales_year , sales_month))/lag(total_sales) over (order by sales_year , sales_month) * 100,2) as MoM_growth_percentage
 from monthly_Sales;
+
+
+/*What is the cumulative/running Sales by month*/
+
+with monthly_sales as 
+(select
+year(
+case
+when `Order Date` like '%/%' then str_to_date (`Order Date` , '%m/%d/%Y')
+when `Order Date` like '%-%' then str_to_date (`Order Date` , '%m-%d-%Y')
+end) as sales_year, 
+month(
+case 
+when `Order Date` like '%/%' then str_to_date(`Order Date`, '%m/%d/%Y')
+WHEN `Order Date` like '%-%' then str_to_date(`Order Date` , '%m-%d-%Y')
+End) as sales_month,
+sum(Sales) as total_sales
+from saless
+group by 1,2)
+
+select 
+sales_year , 
+sales_month,
+total_sales,
+round(sum(total_Sales) over (order by sales_year , sales_month),2) as running_total
+from monthly_Sales;
+
 
